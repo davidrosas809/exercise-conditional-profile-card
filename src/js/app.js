77b796fd29991746e58bb8e12cd574ece8142512
David Rosas <156865176+davidrosas809@users.noreply.github.com>
2025-05-30 03:10:43 +0000

    Finishing my tasks

--- src/js/app.js
+++ src/js/app.js
@@ -33,23 +33,27 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>${variables.name ? "First Name" : variables.name}
-           ${variables.lastname ? "Last Name" : variables.lastName}</h1>
+          <h1>${variables.name == null ? "First Name" : variables.name}
+           ${variables.lastname == null ? "Last Name" : variables.lastName}</h1>
           <h2>${variables.role}</h2>
           <h3>${variables.city}</h3>
           <h2>${variables.country}</h2>
           <ul class=${variables.socialMediaPosition}>
             <li><a href="https://twitter.com/${
-              variables.twitter ? !"Twitter ID" : variables.twitter
+              variables.twitter == null ? !"Twitter ID" : variables.twitter
             }"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/${
-              variables.github ? !"Github ID" : variables.github
+              variables.github == null ? !"Github ID" : variables.github
             }"><i class="fab fa-github"></i></a></li>
             <li><a href="https://linkedin.com/school/${
-              variables.linkedin ? !"Linkedin Profile" : variables.linkedin
+              variables.linkedin == null
+                ? !"Linkedin Profile"
+                : variables.linkedin
             }"><i class="fab fa-linkedin"></i></a></li>
             <li><a href="https://instagram.com/${
-              variables.instagram ? !"Instagram ID" : variables.instagram
+              variables.instagram == null
+                ? !"Instagram ID"
+                : variables.instagram
             }"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
